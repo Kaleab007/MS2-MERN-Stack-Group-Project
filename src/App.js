1692e01b04d49aa64fca832f09206fa7f066e0b4
Kaleab007 <abatenehkaleab@gmail.com>
2022-06-06 18:07:37 -0700

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Home> </Home>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home (){
+    return ( 
+        <div>Home</div>
+
+    ) 
+    
+}
+<main className="Home" role={main}
+id="Home-Page">
+<div class=""
+
+
+
+
+</main>
+export default Home;
+
