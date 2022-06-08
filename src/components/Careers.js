afdb32b587a19da9e2a7b9efa56f3525d7500567
Kaleab007 <abatenehkaleab@gmail.com>
2022-06-07 20:12:36 -0700

    changes

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -20,9 +20,14 @@ function Careers (){
             <a href="content" className="skip-link">Skip to main content</a>
             <header className="page-header">
                 <nav className="nav">
-                    <a href="logo-header-link"></a>
+                    <a href="logo-header-link">company Logo</a>
                 </nav>
             </header>
         </div>
+        <section class="search_bar">Jobs
+        <button class= "search_bar__full-placeholder">
+           <div class="search_bar__full-placeholder">"Campbell,California,United States"</div> 
+           <span class="sr-only">Expend search</span>
+           </button></section>
     </body>
 </html>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,37 +2,38 @@ import React from "react";
 
 function Home (){
     return ( 
-        <div>Home</div>
+        <main class="Home" role="main"
+        id="Home-Page">
+            <section class="box"><div class="    ">
+            <h1 class="Header" id="Intro-header"> Join TruJobz for diverse career to discover!  </h1>
+            <div className="Lists">
+                <ul class="Choices">
+                    <li class="m">
+                    <a class="searchBar" href="links" > 
+                    Search for Careers
+                    <icon class="arrow"> </icon>
+                    </a>
+                    </li>
+                    <li class="m">
+                    <a class="MentorBar" href="links" >Search Mentor
+                    <icon class="arrow"> </icon>
+                    </a>
+                    </li>
+                    <li class="m">
+                    <a class="resumeBar" href="links" > Research 
+                    <icon class="arrow"> </icon>
+                </a>
+                </li>
+                </ul>
+                </div>
+                </div>
+            </section>
+        </main>   
+         
 
     ) 
     
 }
-<main className="Home" role={main}
-id="Home-Page">
-    <section class="box"><div class="    ">
-    <h1 class="Header" id="Intro-header"> Join TruJobz for diverse career to discover!  </h1>
-    <div className="Lists">
-        <ul class="Choices">
-            <li class="m">
-            <a class="searchBar" href="links" > 
-            "Search for Careers"
-            <icon class="arrow"> </icon>
-            </a>
-            </li>
-            <li class="m">
-            <a class="MentorBar" href="links" > "Search mentor"
-            <icon class="arrow"> </icon>
-            </a>
-            </li>
-            <li class="m">
-            <a class="resumeBar" href="links" > "research "
-            <icon class="arrow"> </icon>
-        </a>
-        </li>
-        </ul>
-        </div>
-        </div>
-    </section>
-</main>
+
 export default Home;
 
